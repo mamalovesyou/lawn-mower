a6fe227f1333e4c2da4015cfd02cde8e67c7407e
Matthieu Berger <matthieuberger84@gmail.com>
2019-05-27 17:23:56 +0200

    Added first MVP

---
+++ main.py
@@ -0,0 +1,79 @@
+import logging
+import sys
+
+from mower import Mower
+
+class InputParser:
+
+    DEFAULT_INPUT_FILE_PATH = "input.txt"
+
+    def __init__(self, input_file=None):
+        if input_file:
+            self._input_file = input_file
+        else:
+            self._input_file = self.DEFAULT_INPUT_FILE_PATH
+        
+        self.grid_size = (None, None)
+        self.mowers = []
+        
+    
+    def parse(self):
+        with open(self._input_file, 'r') as fp:
+            # First line is always grid size
+            line = fp.readline()
+            self.grid_size = self.get_grid_size(line)
+            
+            # Load mowers infos (every 2 lines this is a new mower)
+            mowers = fp.readlines()
+            for i in range(0, len(mowers), 2):
+                print(mowers[i], mowers[i+1])
+                initial_position = mowers[i].rstrip()
+                coords, orientation = self.get_mower_position(initial_position)
+                new_mower = Mower(i, coords, orientation, mowers[i+1].rstrip())
+                self.mowers.append(new_mower)
+                
+    
+
+
+    def get_grid_size(self, size_str):
+        """
+        Return the size of a grid as a tuple of integer. (height, width)
+        @param size_str: Size of the grid as string. (ex: '5 5')
+        @return: Tuple of integer (height, width) or None if parsing error
+        """
+        dimensions = size_str.split(' ')
+
+        # Parsing error
+        if len(dimensions) != 2:
+            return (None, None)
+        
+        return tuple(map(int, dimensions))
+    
+    def get_mower_position(self, position_str):
+
+        # Split string using space char
+        position = position_str.split(' ')
+
+        # Parsing error
+        if len(position) != 3:
+            return None, None
+
+        # Mower's coords are 2 first elements in the list
+        coords = tuple(map(int, position[:2]))
+
+        # Orientation is always last in the list
+        orientation = position[-1]
+        return coords, orientation
+    
+
+
+if __name__ == '__main__':
+
+    # Configure logger
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    input_parser = InputParser()
+    input_parser.parse()
+
+    for m in input_parser.mowers:
+        m.process()
---
+++ mower.py
@@ -0,0 +1,81 @@
+import logging
+import operator
+from collections import deque
+
+class Mower:
+    """
+    Mower class
+    """
+     
+    ORIENTATIONS = {'N': (0,1),'E':(1,0),'S':(0,-1),'W':(-1,0)}
+    INSTRUCTIONS = {'L': -1, 'R': +1, 'F':0}
+
+    def __init__(self, identifier, init_position, init_orientation, instructions):
+
+        # Create logger
+        self.name = "Mower-{}".format(identifier)
+        self.logger = logging.getLogger(self.name)
+
+        self._available_orientations = list(self.ORIENTATIONS.keys())
+
+        self.initial_position = init_position
+        self.initial_orientation = init_orientation
+        self.current_position = init_position
+        self.current_orientation = init_orientation
+        self.instructions = instructions
+
+    def process(self):
+        """
+        Implement the logic for the next instruction
+        """
+        # Dequeue list of instructions
+        self.logger.info("Preparing processing intructions")
+        for i in self.instructions:
+            self._process_instruction(i)
+        
+
+    def _process_instruction(self, instruction):
+        """
+        Process instruction
+        """
+        if instruction not in self.INSTRUCTIONS.keys():
+            self.logger.error("Instruction %s unknown", instruction)
+            return
+        
+        # Case where it's a forward instruction
+        if instruction == 'F':
+            return self._process_instruction_forward()
+        
+        # Process a reorientation of the mower
+        index = self._available_orientations.index(self.current_orientation)
+        index += self.INSTRUCTIONS[instruction]
+        if index < 0:
+            index = len(self._available_orientations) - 1
+        elif index >= len(self._available_orientations):
+            index = 0
+        new_orientation = self._available_orientations[index]
+
+        self.logger.error("Performing %s instruction. From: (%s, %s) -> To: (%s, %s)", 
+                            instruction, self.current_position, self.current_orientation, 
+                            self.current_position, new_orientation)
+
+        self.current_orientation = self._available_orientations[index]
+    
+    def _process_instruction_forward(self):
+        """
+        Process forward instructions
+        """
+        coords_increment = self.ORIENTATIONS[self.current_orientation]
+        new_position = tuple(map(operator.add, self.current_position,coords_increment))
+        self.current_position = new_position
+        self.logger.error("Performing F (Forward) instruction. From: (%s, %s) -> To: (%s, %s)", 
+                            self.current_position, self.current_orientation, 
+                            new_position, self.current_orientation)
+        self.current_position = new_position
+        
+        
+
+
+        
+        
+
